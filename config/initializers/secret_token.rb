3d7ef7cef3b2b8394f68162ca760288a8527cbb4
tomomichi <tomomichi.onishi@gmail.com>
2013-12-20 11:58:11 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poolog::Application.config.secret_key_base = '25a04350591a9edf7c8870620f0b6c677fa503c8200e6610ee7e8d2d4c989ccff920bc2a8a973d5eac3355b55b7b9524059dbb3a6e66e4ca50911e71cecb2daf'
