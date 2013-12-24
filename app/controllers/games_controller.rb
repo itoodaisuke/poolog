0f7f95b53316444564ddbdb0dec6ea47b2458606
tomomichi <tomomichi.onishi@gmail.com>
2013-12-24 17:26:45 +0900

    add accepted params to save user-game association

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -69,6 +69,6 @@ class GamesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:game).permit(:name, :date, :place)
+      params.require(:game).permit(:name, :date, :place, :user_ids => [])
     end
 end
