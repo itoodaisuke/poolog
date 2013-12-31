2c3de0c8e9d7e4edcb2ec0f6addb4688c24f7df3
tomomichi <tomomichi.onishi@gmail.com>
2013-12-31 14:07:25 +0900

    title test using helpers

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
 describe "Games" do
-  describe "GET /games" do
+  shared_examples_for "all game pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "index page" do
     before { visit games_path }
     subject { page }
+    let(:heading) { 'Games' }
+    let(:page_title) { 'Games' }
 
-    it { should have_content('Games Index') }
+    it_should_behave_like "all game pages"
+  end
+
+  describe "new page" do
+    before { visit new_game_path }
+    subject { page }
+    let(:heading) { 'New Game' }
+    let(:page_title) { 'New Game' }
+
+    it_should_behave_like "all game pages"
   end
 end
