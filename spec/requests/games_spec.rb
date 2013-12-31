83028f1f85f0fb2d8e08ed16a930c5aa282b2464
tomomichi <tomomichi.onishi@gmail.com>
2013-12-31 20:32:47 +0900

    add pending specs

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -7,27 +7,54 @@ describe "Games" do
   end
 
   describe "index page" do
-    before { visit games_path }
-    subject { page }
-    let(:heading) { 'Games' }
-    let(:page_title) { 'Games' }
+    describe "when not logged in" do
+      before { visit games_path }
+      subject { page }
+      let(:heading) { 'Games' }
+      let(:page_title) { 'Games' }
 
-    it_should_behave_like "all game pages"
+      it_should_behave_like "all game pages"
+      it "should not have edit path" do
+        pending("not deployed yet")
+      end
+    end
+
+    describe "when lgged in" do
+      it "should have right navigation" do
+        pending("not deployed yet")
+      end
+
+      it "should have edit path for its records" do
+        pending("not deployed yet")
+      end
+
+      it "should not have edit path for others records" do
+        pending("not deployed yet")
+      end
+    end
   end
 
   describe "new page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before {
-      visit new_user_session_path
-      fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: user.password
-      click_button 'Sign in'
-      visit new_game_path
-    }
-    subject { page }
-    let(:heading) { 'New Game' }
-    let(:page_title) { 'New Game' }
+    describe "without login" do
+      before { visit new_game_path }
+      subject { page }
+      it { should have_content('Sign in')}
+    end
 
-    it_should_behave_like "all game pages"
+    describe "with valid user" do
+      let(:user) { FactoryGirl.create(:user) }
+      before {
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'Sign in'
+        visit new_game_path
+      }
+      subject { page }
+      let(:heading) { 'New Game' }
+      let(:page_title) { 'New Game' }
+
+      it_should_behave_like "all game pages"
+    end
   end
 end
