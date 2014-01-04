4a19b3e5efcb7ccea0f355d811d9566a15f5d4cf
tomomichi <tomomichi.onishi@gmail.com>
2014-01-04 11:41:45 +0900

    trim youtube video url

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,11 @@ class Game < ActiveRecord::Base
   has_many :game_records
   has_many :users, through: :game_records
   accepts_nested_attributes_for :game_records
+
+  before_save do
+    if self.video_id.present?
+      id = self.video_id.match(%r{http://www.youtube.com/.*[\?|&]v=([a-zA-Z0-9_-]{11})&*})
+      self.video_id = id ? id[1] : nil
+    end
+  end
 end
