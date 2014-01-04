b6fe5c02ac535ece9ac33599caee8991a0512987
tomomichi <tomomichi.onishi@gmail.com>
2014-01-04 13:44:40 +0900

    modify show page design

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,12 @@
 class Game < ActiveRecord::Base
   has_many :game_records
   has_many :users, through: :game_records
+  belongs_to :place
   accepts_nested_attributes_for :game_records
+
+  # validates :rule, presence: true
+  # validates :date, presence: true
+  # validates :video_id, format: {with: %r{http://www.youtube.com/.*[\?|&]v=([a-zA-Z0-9_-]{11}).*}}
 
   before_save do
     if self.video_id.present?
