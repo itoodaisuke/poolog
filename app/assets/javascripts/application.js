5cec639ad6dc588b66c16f1352bd128bdadef5a1
tomomichi <tomomichi.onishi@gmail.com>
2014-01-07 22:33:49 +0900

    install jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require jquery.sidr.min
+//= require turbolinks
 //= require_tree .
