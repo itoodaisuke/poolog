849f8775d281d40f2fb10b00c964d3aa1700ad65
kayhide <kayhide@gmail.com>
2014-01-29 10:20:23 +0900

    replace Rspec by RSpec.

--- spec/support/matchers/require_login.rb
+++ spec/support/matchers/require_login.rb
@@ -1,4 +1,4 @@
-Rspec::Matchers.define :require_login do |expected|
+RSpec::Matchers.define :require_login do |expected|
   match do |actual|
     expect(actual).to redirect_to Rails.application.routes.url_helpers.new_user_session_path
   end
